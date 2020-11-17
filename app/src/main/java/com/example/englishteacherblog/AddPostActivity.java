4f2cadd4f116945afbbd4f6a16626228d6d2dbb1
SirMikl <sirmikl.aleinikov@gmail.com>
2020-11-17 09:44:34 +0300

    Added AddPostActivity

--- app/src/main/java/com/example/englishteacherblog/AddPostActivity.java
+++ app/src/main/java/com/example/englishteacherblog/AddPostActivity.java
@@ -1,24 +1,100 @@
 package com.example.englishteacherblog;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
 
 public class AddPostActivity extends AppCompatActivity {
     EditText title_blog, description_blog;
     Button upload;
     ImageView blog_image;
+    Uri image_uri = null;
+    public static final int GALLERY_IMAGE_CODE = 100;
+    public static final int CAMERA_IMAGE_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_post);
+        permission();
         title_blog = findViewById(R.id.title_blog);
         description_blog=findViewById(R.id.desciption_blog);
         upload = findViewById(R.id.uploadBtn);
         blog_image = findViewById(R.id.blog_image);
+        
+        blog_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imagePickDialog();
+            }
+        });
+    }
+
+    private void imagePickDialog() {
+        String[] options = {"Camera", "Gallery"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Choose image from");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which==0){
+                    galleryPick();
+                }
+                if (which==1){
+                    cameraPick();
+                }
+            }
+        });
+        builder.create().show();
+    }
+
+    private void cameraPick() {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.TITLE, "Temp Pick");
+        contentValues.put(MediaStore.Images.Media.TITLE, "Temp desc");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+    }
+
+    private void galleryPick() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, GALLERY_IMAGE_CODE);
+    }
+
+    private void permission(){
+        Dexter.withContext(this)
+                .withPermissions(
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ).withListener(new MultiplePermissionsListener() {
+            @Override
+            public void onPermissionsChecked(MultiplePermissionsReport report) {
+
+            }
+            @Override
+            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+
+            }
+        }).check();
     }
 }
