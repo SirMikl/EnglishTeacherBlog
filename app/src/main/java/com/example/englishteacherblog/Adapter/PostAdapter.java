1f7f5570349eb03a73a4925d2f7918202f4267e5
SirMikl <sirmikl.aleinikov@gmail.com>
2020-11-18 12:05:43 +0300

    Added AddPostActivity Relize of AddPostActivity Added Model class and adapter

---
+++ app/src/main/java/com/example/englishteacherblog/Adapter/PostAdapter.java
@@ -0,0 +1,62 @@
+package com.example.englishteacherblog.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.englishteacherblog.Model.PostModel;
+import com.example.englishteacherblog.R;
+
+import java.util.List;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.MyHolder> {
+    Context context;
+    List<PostModel> postModelList;
+
+    public PostAdapter(Context context, List<PostModel> postModelList) {
+        this.context = context;
+        this.postModelList = postModelList;
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.home_post, parent, false);
+        return new MyHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+        String title = postModelList.get(position).getpTitle();
+        String description = postModelList.get(position).getpDescription();
+        String image = postModelList.get(position).getpImage();
+        holder.postTitle.setText(title);
+        holder.postDescription.setText(description);
+        Glide.with(context).load(image).into(holder.postImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return postModelList.size();
+    }
+
+    class MyHolder extends RecyclerView.ViewHolder{
+        ImageView postImage;
+        TextView postTitle, postDescription;
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+            postImage = itemView.findViewById(R.id.postImage);
+            postTitle = itemView.findViewById(R.id.postTitle);
+            postDescription = itemView.findViewById(R.id.postDescription);
+        }
+    }
+
+}
--- app/src/main/java/com/example/englishteacherblog/HomeActivity.java
+++ app/src/main/java/com/example/englishteacherblog/HomeActivity.java
@@ -2,22 +2,60 @@ package com.example.englishteacherblog;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
+import com.example.englishteacherblog.Adapter.PostAdapter;
+import com.example.englishteacherblog.Model.PostModel;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
     FirebaseAuth auth;
+    RecyclerView recyclerView;
+    PostAdapter postAdapter;
+    List<PostModel> postModelList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         auth = FirebaseAuth.getInstance();
+        recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setStackFromEnd(true);
+        layoutManager.setReverseLayout(true);
+        recyclerView.setLayoutManager(layoutManager);
+        postModelList = new ArrayList<>();
+        loadPosts();
+    }
+
+    private void loadPosts() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Posts");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
