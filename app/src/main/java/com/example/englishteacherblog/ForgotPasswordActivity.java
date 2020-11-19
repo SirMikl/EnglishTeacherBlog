6c1da363cb0b1cfdfe8bfc1021c2f8f6ad40f422
SirMikl <sirmikl.aleinikov@gmail.com>
2020-11-19 14:25:21 +0300

    Added AddPostActivity Relize of AddPostActivity Added Model class and adapter finished adapter added Recover pass activity_forgot_password.xml finished main app

--- app/src/main/java/com/example/englishteacherblog/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/englishteacherblog/ForgotPasswordActivity.java
@@ -1,14 +1,21 @@
 package com.example.englishteacherblog;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
@@ -16,6 +23,7 @@ public class ForgotPasswordActivity extends AppCompatActivity {
     EditText email;
     Button recoverPass;
     FirebaseAuth auth;
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,7 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         email = findViewById(R.id.forgotPass);
         recoverPass = findViewById(R.id.forgotBtn);
         auth = FirebaseAuth.getInstance();
+        pd = new ProgressDialog(this);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Forgot password");
@@ -39,13 +48,29 @@ public class ForgotPasswordActivity extends AppCompatActivity {
                 } else {
                     recoverPassword(etEmail);
                 }
-
             }
         });
 
     }
 
     private void recoverPassword(String email) {
-        auth.sendPasswordResetEmail(email);
+        pd.setMessage("Please wait");
+        pd.show();
+        auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    pd.dismiss();
+                    Toast.makeText(ForgotPasswordActivity.this, "Please check email link", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(ForgotPasswordActivity.this, MainActivity.class));
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                pd.dismiss();
+                Toast.makeText(ForgotPasswordActivity.this, ""+e, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
